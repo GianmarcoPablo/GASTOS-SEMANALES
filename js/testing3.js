d6c0586e32453e9c0dfeeb4adc2b6fe7e7a2f2dd
Gian Marco Pablo Totocayo <gianmarcopablo588@gmail.com>
2023-04-03 22:14:16 -0500

    actualizacion

--- js/testing3.js
+++ js/testing3.js
@@ -8,17 +8,20 @@ function cargarEventListeners(){
     formulario.addEventListener("submit",agregarGasto)
 }
 
-
 class Presupuesto{
     constructor(presupuesto){
-        this.presupuesto = Number(presupuesto)
+        this.presupuesto = Number(presupuesto) 
         this.restante = Number(presupuesto)
         this.gastos = []
+    }
+    nuevoGasto(gasto){
+        this.gastos = [...this.gastos,gasto]
+        console.log(this.gastos)
     }
 }
 
 class UI{
-    insertarPresupuest(cantidad){
+    insertarPresupuesto(cantidad){
         const {presupuesto,restante} = cantidad
         document.querySelector("#total").textContent = presupuesto
         document.querySelector("#restante").textContent = restante
@@ -42,27 +45,32 @@ class UI{
     }
 }
 
+const ui = new UI
+
 let presupuesto;
-const ui = new UI()
 function preguntarPresupuesto(){
-    const presupuestoUsuario = prompt("¿Cual es tu presupuesto?")
-    if(presupuestoUsuario === "" || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0){
+    const presupuestoUsuario = prompt("¿Cual es tu presuspuestp?")
+    if(presupuestoUsuario === "" || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0 ){
         window.location.reload()
     }
     presupuesto = new Presupuesto(presupuestoUsuario)
-    ui.insertarPresupuest(seguro)
+    ui.insertarPresupuesto(presupuesto)
+    console.log(presupuesto)
 }
 
 function agregarGasto(e){
     e.preventDefault()
+
     const nombre = document.querySelector("#gasto").value
     const cantidad = document.querySelector("#cantidad").value
 
     if(nombre === "" || cantidad === ""){
-        ui.imprimirAlerta("Todos los campos son obligatorios","error")
+        ui.imprimirAlerta("Todos los mensajes son obligatorios","error")
     }else if(cantidad <= 0 || isNaN(cantidad)){
-        ui.imprimirAlerta("Cantidad no validad","error")
+        ui.imprimirAlerta("Cantidad no valida","error")
     }else{
-        console.log("todo correcto")
+        const gastos = {nombre,cantidad,id: Date.now()}
+        presupuesto.nuevoGasto(gastos)
+        ui.imprimirAlerta("Gasto agregado")
     }
 }
